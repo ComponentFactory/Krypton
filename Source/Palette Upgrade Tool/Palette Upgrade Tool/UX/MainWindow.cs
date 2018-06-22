ca0a8b0dd745766a328d58a8479a321499e65da8
Peter Wagner <peterwagner@live.co.uk>
2018-06-22 11:20:52 +0100

    * Palette upgrade tool

---
+++ Source/Palette Upgrade Tool/Palette Upgrade Tool/UX/MainWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PaletteUpgradeTool.UX
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
