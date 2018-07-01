eeca2a2a0cc6243a1bcfcee8fa8791951117f626
Peter Wagner <peterwagner@live.co.uk>
2018-07-01 11:04:36 +0100

    Reinstated designer code (#135)
    * * Decoupled the designer DLL from rest of toolkit
    
    * * Remove BIN folder
    
    * * Update README.md
    
    * * In development
    
    * * Redo
    
    * * Palette upgrade tool
    
    * * Remove locks
    
    * * Removal of app
    
    * * Working Design.dll
    
    * * Remove build directory
    
    * * Remove VS blocks

---
+++ Source/Palette Upgrade Tool/Palette Upgrade Tool/Program.cs
@@ -0,0 +1,20 @@
+﻿using PaletteUpgradeTool.UX;
+using System;
+using System.Windows.Forms;
+
+namespace PaletteUpgradeTool
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainWindow());
+        }
+    }
+}
